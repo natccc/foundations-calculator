773b00cb634b93176de3211773052066cc60745a
Natalie <natchc@gmail.com>
2025-08-10 19:51:27 +0100

    add initial logic

---
+++ main.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+let num1;
+let num2;
+let operator;
+
+function operate(num1, num2, operator) {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+  }
+}
