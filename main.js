d228743850f45babf8967578e4d5a81062b96992
Natalie <natchc@gmail.com>
2025-08-11 10:39:53 +0100

    Display one operator at a time and add 0 as intial display"

--- main.js
+++ main.js
@@ -1,3 +1,71 @@
+let operandOne;
+let operandTwo;
+let operator;
+let shouldResetDisplay = false;
+
+const display = document.querySelector(".display");
+
+const digitButtons = document.querySelectorAll(".digit-btn");
+digitButtons.forEach((button) =>
+  button.addEventListener("click", (e) => inputDigit(e))
+);
+
+const equalBtn = document.querySelector("#equal-btn");
+equalBtn.addEventListener("click", equal);
+
+const operatorBtn = document.querySelectorAll(".operator");
+operatorBtn.forEach((button) =>
+  button.addEventListener("click", (e) => inputOperator(e))
+);
+
+const allClearBtn = document.querySelector("#all-clear");
+allClearBtn.addEventListener("click", allClear);
+
+function inputDigit(e) {
+  if (shouldResetDisplay) {
+    display.innerText = e.target.innerText;
+    shouldResetDisplay = false;
+  } else {
+    display.innerText === "0"
+      ? (display.innerText = e.target.innerText)
+      : (display.innerText += e.target.innerText);
+  }
+}
+
+function inputOperator(e) {
+  operandOne = display.innerText;
+  operator = e.target.innerText;
+  shouldResetDisplay = true;
+  console.log(operandOne, operator);
+}
+
+function equal() {
+  operandTwo = display.innerText;
+  display.innerText = operate(operandOne, operator, operandTwo);
+}
+
+function operate(operandOne, operator, operandTwo) {
+  operandOne = Number(operandOne);
+  operandTwo = Number(operandTwo);
+  switch (operator) {
+    case "+":
+      return add(operandOne, operandTwo);
+    case "-":
+      return subtract(operandOne, operandTwo);
+    case "×":
+      return multiply(operandOne, operandTwo);
+    case "÷":
+      return divide(operandOne, operandTwo);
+  }
+}
+
+function allClear() {
+  operandOne = null;
+  operandTwo = null;
+  operator = null;
+  display.innerText = "0";
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -11,26 +79,6 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+  if (b === 0) return "ERROR";
   return a / b;
-}
-let num1;
-let num2;
-let operator;
-
-function operate(num1, num2, operator) {
-  num1 = Number(num1);
-  num2 = Number(num2);
-  switch (operator) {
-    case "+":
-      add(num1, num2);
-      break;
-    case "-":
-      subtract(num1, num2);
-      break;
-    case "*":
-      multiply(num1, num2);
-      break;
-    case "/":
-      divide(num1, num2);
-  }
 }
